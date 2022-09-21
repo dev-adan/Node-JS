0cf29c1b621275ce1a8e4eddcdb7148fbf804a1f
Adan <adanaligakhar@gmail.com>
2022-09-21 21:51:22 +0500

    avacado project

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,23 +1,109 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url')
 
 //Blocking synchronous way
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('file written');
 
-//Blocking asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) => {
-        console.log(data2)
-        fs.readFile('./txt/append.txt', 'utf-8', (err,data3) => {
-            console.log(data3)
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err => {
-                console.log('You file has been written')
-            })
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('file written');
+
+// //Blocking asynchronous way
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) => {
+//         console.log(data2)
+//         fs.readFile('./txt/append.txt', 'utf-8', (err,data3) => {
+//             console.log(data3)
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err => {
+//                 console.log('You file has been written')
+//             })
+//         });
+//     });
+// });
+
+
+// console.log('async in action, will read file')
+
+
+/////////////       Server                 ///////
+
+const replaceTemplate = (temp,product) => {
+
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic){
+         output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+    }
+
+    return output;
+   
+}
+
+const tempOverview =fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard =fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct =fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
+const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req,res) => {
+   
+   
+    const {query,pathname} = url.parse(req.url,true)
+
+
+    if(pathname === '/' || pathname === '/overview'){
+
+        res.writeHead(200, {
+            'Content-type' : 'text/html'
         });
-    });
+        
+        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard,el));
+        
+        let output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHTML)
+        res.end(output)
+
+    }
+    
+    
+    else if (pathname === '/product'){
+        const product = dataObj[query.id];
+        res.writeHead(200, {
+            'Content-type' : 'text/html'
+        });
+        const output = replaceTemplate(tempProduct,product)
+        res.end(output)
+        
+
+    }else if(pathname === '/api'){
+
+        res.writeHead(200,{
+            'Content-type' : 'application/json'
+           })
+           res.end(data);
+        
+    }
+    else{
+        res.writeHead(404,{
+            'Content-type' : 'text/html',
+            'my-own-header' : 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>')
+    }
+
+
 });
 
-
-console.log('async in action, will read file')
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
