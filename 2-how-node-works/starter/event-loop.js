6ca5fd7730a0c8e2de7285fd7b18855219f1fa20
Adan <adanaligakhar@gmail.com>
2022-09-22 14:59:25 +0500

    event loop , code blocking

---
+++ 2-how-node-works/starter/event-loop.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const crypto = require('crypto');
+const start = Date.now();
+
+setTimeout(() => {
+  console.log("Timer 1 finished");
+}, 0);
+
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/0 finished");
+
+  setTimeout(() => {
+    console.log("Timer 2 finished");
+  }, 0);
+
+  setTimeout(() => {
+    console.log("Timer 3 finished");
+  }, 3000);
+
+  setImmediate(() => console.log("Immediate 2 finished"));
+
+  process.nextTick(() => console.log("process.nextTick"));
+
+  crypto.pbkdf2('password','salt',100000,1024,'sha512', () =>{
+    console.log( Date.now() - start,'password encrypted')
+  })
+
+  crypto.pbkdf2('password','salt',100000,1024,'sha512', () =>{
+    console.log( Date.now() - start,'password encrypted')
+  })
+
+  crypto.pbkdf2('password','salt',100000,1024,'sha512', () =>{
+    console.log( Date.now() - start,'password encrypted')
+  })
+
+  crypto.pbkdf2('password','salt',100000,1024,'sha512', () =>{
+    console.log( Date.now() - start,'password encrypted')
+  })
+
+
+
+});
+
+console.log("Hello from the top level-code");
