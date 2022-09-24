80353c3041bd4c71577f3fa44ddf9002736f8e35
Adan <adanaligakhar@gmail.com>
2022-09-25 00:32:28 +0500

    express

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const fs = require('fs');
+
+
+const app = express();
+app.use(express.json()); //middleWare
+
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+
+app.get('/api/v1/tours/:id',(req,res) => {
+
+    
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    res.status(200).json({
+        status : 'success',
+        results : tours.length,
+        data : {
+            tour
+        }
+
+    })
+});
+
+app.get('/api/v1/tours',(req,res) => {
+    res.status(200).json({
+        status : 'success',
+        results : tours.length,
+        data : {
+            tours,
+        }
+
+    })
+});
+
+
+app.post('/api/v1/tours',(req,res) => {
+    
+
+    const newId = tours[tours.length - 1].id + 1;
+
+    const newTour = Object.assign({id : newId},req.body);
+    tours.push(newTour);
+
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err => {
+       res.status(201).json({
+        status : 'success',
+        data : {
+            tour : newTour,
+        }
+       })
+    })
+})
+
+app.patch('/api/v1/tours/:id',(req,res) => {
+    
+})
+
+ 
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+})
+
